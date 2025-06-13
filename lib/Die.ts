203fb5b61302e908c8f16d3f85b5efce186ae073
Miguel Rojas <leprosy57@gmail.com>
2025-06-13 16:04:04 -0400

    Added straight detection, fixed some bugs

--- lib/Die.ts
+++ lib/Die.ts
@@ -5,6 +5,12 @@ export enum DieMaterial {
   Bronze,
 }
 
+export type Hand = {
+  name: string;
+  base: number;
+  mult: number;
+};
+
 export class Die {
   value: number;
   maxValue: number;
@@ -24,11 +30,6 @@ export class Die {
 export class Dice {
   dice: Die[];
 
-  constructor(totalDice: number) {
-    this.dice = [];
-    for (let i = 0; i < totalDice; ++i) this.dice.push(new Die());
-  }
-
   static getCountArr(data: [string, number][]) {
     const formatted = data.map((item) => {
       return { rank: parseInt(item[0]), count: item[1] };
@@ -41,32 +42,47 @@ export class Dice {
       if (countDiff !== 0) {
         return countDiff;
       } else {
-        return b.rank - a.rank;
+        // TODO: will be higher than 10 die?
+        return (b.rank === 1 ? 10 : b.rank) - (a.rank === 1 ? 10 : a.rank); // aces are higher
       }
     });
   }
 
+  constructor(totalDice: number) {
+    this.dice = [];
+    for (let i = 0; i < totalDice; ++i) this.dice.push(new Die());
+  }
+
+  setDice(arr: number[]) {
+    arr.forEach((value, i) => (this.dice[i].value = arr[i]));
+  }
+
   roll() {
     this.dice.forEach((die) => die.roll());
-    console.log(
-      "roll",
-      this.dice.map((i) => i.value)
-    );
+    return JSON.stringify(this.dice.map((i) => i.value)); // debug
   }
 
   getHand() {
+    /*
+    pair
+    2 pairs
+    3 of a kind
+    pichanga
+    straight
+    full house
+    4 of a kind
+    nocaut
+    */
     const counts = this.dice.reduce((acc: Record<number, number>, cur: Die) => {
       if (!acc[cur.value]) acc[cur.value] = 0;
       acc[cur.value] += 1;
       return acc;
     }, {});
 
-    // Sort by count, then by rank
-    console.log(Object.entries(counts));
     const countArr = Dice.getCountArr(Object.entries(counts));
-    console.log("countArr", countArr);
 
-    if (countArr[0].count >= 5) {
+    // TODO: this algorithm works for 5 dice
+    if (countArr[0].count === this.dice.length) {
       return `nocaut ${countArr[0].rank}`;
     }
 
@@ -76,7 +92,7 @@ export class Dice {
 
     if (countArr[0].count === 3) {
       if (countArr[1].count === 2) {
-        return `full house ${countArr[0].count}/${countArr[1].count}`;
+        return `full house ${countArr[0].rank}/${countArr[1].rank}`;
       }
       return `three of a kind ${countArr[0].rank}`;
     }
@@ -88,6 +104,13 @@ export class Dice {
       return `pair ${countArr[0].rank}`;
     }
 
-    return "none";
+    let isStraight = true;
+    countArr.forEach((item: { rank: number; count: number }, i: number) => {
+      if (i > 0 && item.rank !== countArr[i - 1].rank - 1) {
+        isStraight = false;
+      }
+    });
+
+    return isStraight ? "straight" : "???";
   }
 }
